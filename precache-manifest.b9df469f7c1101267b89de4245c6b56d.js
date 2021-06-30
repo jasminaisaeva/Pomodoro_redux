6ad1e371324eeadd2da12c64ce810551b0015239
jasminaisaeva <jasminaisaeva2001@gmail.com>
2021-06-30 11:09:15 +0600

    Updates

---
+++ precache-manifest.b9df469f7c1101267b89de4245c6b56d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da0488ac791e1643908fdb1ebb5df7de",
+    "url": "/Pomodoro_redux/index.html"
+  },
+  {
+    "revision": "59c8beff1cb22b7a4093",
+    "url": "/Pomodoro_redux/static/css/main.447fc28c.chunk.css"
+  },
+  {
+    "revision": "2c6b6551e1a3895ab044",
+    "url": "/Pomodoro_redux/static/js/2.8d744b23.chunk.js"
+  },
+  {
+    "revision": "aae0757f3c3679f699f5d39bc18f0dca",
+    "url": "/Pomodoro_redux/static/js/2.8d744b23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59c8beff1cb22b7a4093",
+    "url": "/Pomodoro_redux/static/js/main.dcb5d606.chunk.js"
+  },
+  {
+    "revision": "b39e9419e1e93e8851ef",
+    "url": "/Pomodoro_redux/static/js/runtime-main.790b6611.js"
+  },
+  {
+    "revision": "525320df0b1f39bb54c3662d70cde5c6",
+    "url": "/Pomodoro_redux/static/media/bell.525320df.flac"
+  }
+]);
